3aa15358a60ad775f532da21c4a81ff49bf5faf0
George gd Fekaris <gdfekaris@gmail.com>
2019-03-02 22:21:57 -0800

    Trying to connect Mongo on EC2

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 //const mongo = require('../MongoDB/index.js');
 
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+//const assert = require('assert');
 //const url = 'mongodb://localhost:27017';
 const url = 'mongodb://54.215.246.57:27017';
 const dbName = 'menu-bar-data';
