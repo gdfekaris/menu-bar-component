8a4df423cc83f0427fbfe2223e7ca440556e5138
George gd Fekaris <gdfekaris@gmail.com>
2019-03-09 11:16:09 -0800

    port change

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -1,8 +1,8 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 //const url = 'mongodb://localhost:27017';
-//const url = 'mongodb://172.31.2.0:27017';
-const url = 'mongodb://54.67.109.46:27017';
+const url = 'mongodb://172.31.2.0:27017';
+//const url = 'mongodb://54.67.109.46:27017';
 const client = new MongoClient(url, { useNewUrlParser: true });
 
 client.connect(function (err) {
--- MongoDB/seedScript.js
+++ MongoDB/seedScript.js
@@ -1,8 +1,8 @@
 const MongoClient = require('mongodb').MongoClient;
 const faker = require('faker');
 //const url = 'mongodb://localhost:27017';
-//const url = 'mongodb://172.31.2.0:27017';
-const url = 'mongodb://54.67.109.46:27017';
+const url = 'mongodb://172.31.2.0:27017';
+//const url = 'mongodb://54.67.109.46:27017';
 const client = new MongoClient(url, { useNewUrlParser: true });
 const dbName = 'menu-bar-data';
 const usersCollection = 'users';
@@ -68,6 +68,16 @@ module.exports = () => {
   });
   return `--------------------------\nMongoDB seed script called\n--------------------------\n`;
 }
+
+// module.exports = () => {
+//   client.connect(function (err) {
+//     if (err) {
+//       console.log(err)
+//     } else {
+//       console.log(`Connected to MongoDB`);
+//     }
+//   })
+// }
 
 require('make-runnable/custom')({
   printOutputFrame: false
