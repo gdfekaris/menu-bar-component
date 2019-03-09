1e57088c52b02ab829f6ba06004adb8821ec5664
George gd Fekaris <gdfekaris@gmail.com>
2019-03-09 00:33:35 -0800

    port change

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -1,8 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
-const url = 'mongodb://localhost:27017';
-//const url = 'mongodb://54.215.246.57:27017';
-const dbName = 'menu-bar-data';
+//const url = 'mongodb://localhost:27017';
+const url = 'mongodb://172.31.2.0:27017';
 const client = new MongoClient(url, { useNewUrlParser: true });
 
 client.connect(function (err) {
--- MongoDB/seedScript.js
+++ MongoDB/seedScript.js
@@ -1,7 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 const faker = require('faker');
 //const url = 'mongodb://localhost:27017';
-const url = 'mongodb://54.215.246.57:27017';
+const url = 'mongodb://172.31.2.0:27017';
 const client = new MongoClient(url, { useNewUrlParser: true });
 const dbName = 'menu-bar-data';
 const usersCollection = 'users';
