64f9c1f637780949d3c78b3231f38a43f82fc799
George gd Fekaris <gdfekaris@gmail.com>
2019-03-09 10:04:26 -0800

    port change

--- MongoDB/seedScript.js
+++ MongoDB/seedScript.js
@@ -1,7 +1,8 @@
 const MongoClient = require('mongodb').MongoClient;
 const faker = require('faker');
 //const url = 'mongodb://localhost:27017';
-const url = 'mongodb://172.31.2.0:27017';
+//const url = 'mongodb://172.31.2.0:27017';
+const url = 'mongodb://54.67.109.46:27017';
 const client = new MongoClient(url, { useNewUrlParser: true });
 const dbName = 'menu-bar-data';
 const usersCollection = 'users';
