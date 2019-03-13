66e90b85904fe3a371fd69da0ae2c101338d2021
George gd Fekaris <gdfekaris@gmail.com>
2019-03-13 16:47:39 -0700

    Re-enabled cluster

--- server/index.js
+++ server/index.js
@@ -23,26 +23,26 @@ let mongo = MongoClient.connect(url, {
     }
   })
 
-// const cluster = require('cluster')
-// const { cpus } = require('os')
-// //const log = require('./modules/log')
+const cluster = require('cluster')
+const { cpus } = require('os')
+//const log = require('./modules/log')
 
-// const isMaster = cluster.isMaster
-// const numWorkers = cpus().length
+const isMaster = cluster.isMaster
+const numWorkers = cpus().length
 
-// if (isMaster) {
+if (isMaster) {
 
-//   console.log(`Forking ${numWorkers} workers`)
-//   const workers = [...Array(numWorkers)].map(_ => cluster.fork())
+  console.log(`Forking ${numWorkers} workers`)
+  const workers = [...Array(numWorkers)].map(_ => cluster.fork())
 
-//   cluster.on('online', (worker) => console.log(`Worker ${worker.process.pid} is online`))
-//   cluster.on('exit', (worker, exitCode) => {
-//     console.log(`Worker ${worker.process.id} exited with code ${exitCode}`)
-//     console.log(`Starting a new worker`)
-//     cluster.fork()
-//   })
+  cluster.on('online', (worker) => console.log(`Worker ${worker.process.pid} is online`))
+  cluster.on('exit', (worker, exitCode) => {
+    console.log(`Worker ${worker.process.id} exited with code ${exitCode}`)
+    console.log(`Starting a new worker`)
+    cluster.fork()
+  })
 
-// } else {
+} else {
 
 
 const port = 3015 || process.env.PORT;
@@ -172,7 +172,7 @@ app.get('/username', (req, res) => {
 
 app.listen(port, () => console.log(`Menu Bar server listening on port ${port}!`));
 
-//}
+}
 
 
 
